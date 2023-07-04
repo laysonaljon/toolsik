5d7c13c2cb6a36793f10e2519d352072045da1eb
Aljon Layson <aljonlayson21@gmail.com>
2023-07-05 07:07:57 +0800

    Updated Images

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -21,37 +21,6 @@ export const tools = [
       image: qrgen,
       link: "qrgen"
     },
-    {
-      name: "Youtube Downloader",
-      description:"Download Youtube Videos",
-      image: ytdl,
-      link: "ytdl"
-    },
-    {
-      name: "PDF to Word",
-      description:"Easily convert PDF files into editable Microsoft Word documents, preserving the original layout and formatting, for seamless editing and modification.",
-      image: pdf2word,
-      link: "pdf2word"
-    },
-    {
-      name: "Image to Text",
-      description:"Extract text from images or scanned documents using OCR technology to convert it into editable and searchable text, facilitating efficient handling, editing, and storage of textual information.",
-      image: img2text,
-      link: "img2text"
-
-    },
-    {
-      name: "QR Code Generator",
-      description:"Generate custom QR codes effortlessly to share various types of information and make it easy for users to scan and decode using smartphones or QR code scanners.",
-      image: qrgen,
-      link: "qrgen"
-    },
-    {
-      name: "Youtube Downloader",
-      description:"Download Youtube Videos",
-      image: ytdl,
-      link: "ytdl"
-    },
   ];
 
 
